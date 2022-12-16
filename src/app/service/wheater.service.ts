b950293ac4a914ff9095cd2930b530329a65cbd2
= <=>
2022-12-16 00:25:43 -0500

    changing the temperature to degrees celsius

--- src/app/service/wheater.service.ts
+++ src/app/service/wheater.service.ts
@@ -8,7 +8,7 @@ export class WheaterService {
   URI: string='';
   apiKey = '3b1717495d888a3e58e122b8c0f6a0bf';
   constructor(private httpClient:HttpClient) { 
-    this.URI=`https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&q=`;
+    this.URI=`https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
   }
 
   getWeather(nameCity:string){

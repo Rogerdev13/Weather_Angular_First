cfe3a8f31da5e01bbde919b9a879f993b7d12252
= <=>
2022-12-16 00:19:18 -0500

    Initializing respository

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { WheaterService } from './service/wheater.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'firstApp';
+export class AppComponent implements OnInit {
+  currentWheater:any;
+
+  constructor(private wheaterService: WheaterService){
+
+  }
+  ngOnInit(){
+   
+  }
+
+  getWheaterApi(cityName:string){
+    this.wheaterService.getWeather(cityName)
+      .subscribe(res => this.currentWheater = res , err => console.log(err))
+  }
+
+  submitLocation(cityName:HTMLInputElement){
+    if(cityName.value){
+      this.getWheaterApi(cityName.value)
+      cityName.value = '';
+    }else{
+      alert('Please type something');
+    }
+    cityName.focus();
+    return false
+  }
 }
---
+++ src/app/service/wheater.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WheaterService {
+  URI: string='';
+  apiKey = '3b1717495d888a3e58e122b8c0f6a0bf';
+  constructor(private httpClient:HttpClient) { 
+    this.URI=`https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&q=`;
+  }
+
+  getWeather(nameCity:string){
+    return this.httpClient.get(`${this.URI}${nameCity}`)
+  }
+}
